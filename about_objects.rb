26c14ec589fee7235a078bd68efec36070d97527
Robert <teskadera@gmail.com>
2015-12-08 16:46:18 -0800

    Finish AboutObjects

--- about_objects.rb
+++ about_objects.rb
@@ -2,49 +2,52 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutObjects < Neo::Koan
   def test_everything_is_an_object
-    assert_equal __, 1.is_a?(Object)
-    assert_equal __, 1.5.is_a?(Object)
-    assert_equal __, "string".is_a?(Object)
-    assert_equal __, nil.is_a?(Object)
-    assert_equal __, Object.is_a?(Object)
+    # Isn't this a discouraged practice - checking object class - given 
+    # that Ruby allows/encourages duck-typing?
+    assert 1.is_a?(Object)
+    assert 1.5.is_a?(Object)
+    assert "string".is_a?(Object)
+    assert nil.is_a?(Object)
+    assert Object.is_a?(Object)
   end
 
   def test_objects_can_be_converted_to_strings
-    assert_equal __, 123.to_s
-    assert_equal __, nil.to_s
+    assert_equal "123", 123.to_s
+    assert_equal "", nil.to_s
   end
 
   def test_objects_can_be_inspected
-    assert_equal __, 123.inspect
-    assert_equal __, nil.inspect
+    assert_equal "123", 123.inspect
+    assert_equal "nil", nil.inspect
   end
 
   def test_every_object_has_an_id
     obj = Object.new
-    assert_equal __, obj.object_id.class
+    assert_equal Fixnum, obj.object_id.class
   end
 
   def test_every_object_has_different_id
     obj = Object.new
     another_obj = Object.new
-    assert_equal __, obj.object_id != another_obj.object_id
+    assert obj.object_id != another_obj.object_id
   end
 
   def test_small_integers_have_fixed_ids
-    assert_equal __, 0.object_id
-    assert_equal __, 1.object_id
-    assert_equal __, 2.object_id
-    assert_equal __, 100.object_id
+    assert_equal 0.object_id, 0.object_id
+    assert_equal 1.object_id, 1.object_id
+    assert_equal 2.object_id, 2.object_id
+    assert_equal 100.object_id, 100.object_id
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
+    # Small integers share the same object_id among the same numbers
   end
 
   def test_clone_creates_a_different_object
     obj = Object.new
     copy = obj.clone
 
-    assert_equal __, obj           != copy
-    assert_equal __, obj.object_id != copy.object_id
+    assert obj != copy
+    assert obj.object_id != copy.object_id
   end
 end
