ca61247904a09ec825bad9587ae87ded8e3c5f44
Robert <teskadera@gmail.com>
2015-12-11 13:41:15 -0800

    Finish Triangle work for AboutTriangleProject1, bit for AboutTriangleProject2

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,42 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  Triangle.new(a, b, c).type
 end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+end
+
+class Triangle
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    if (a <= 0 || b <= 0 || c <= 0)
+      raise TriangleError, "A side is impossibly short"
+    end
+    @a, @b, @c = a, b, c
+  end
+
+  def type
+    if is_equilateral?
+      return :equilateral
+    elsif is_isoceles?
+      return :isosceles
+    elsif is_scalene?
+      return :scalene
+    end    
+  end
+
+  private
+  def is_equilateral?()
+    (a == b && b == c)
+  end
+
+  def is_isoceles?()
+    ((a == b && b != c) || (a == c && a != b) || (b == c && a != b))
+  end
+
+  def is_scalene?()
+    (a != b && b != c)
+  end
 end
