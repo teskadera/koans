4240579d53bdd15d24a815088e60febde8c91bb8
Robert <teskadera@gmail.com>
2015-12-10 17:42:58 -0800

    Expand answers in AboutConstants

--- about_constants.rb
+++ about_constants.rb
@@ -7,16 +7,19 @@ class AboutConstants < Neo::Koan
   C = "nested"
 
   def test_nested_constants_may_also_be_referenced_with_relative_paths
-    assert_equal __, C
+    assert_equal "nested", C
   end
 
   def test_top_level_constants_are_referenced_by_double_colons
-    assert_equal __, ::C
+    assert_equal "top level", ::C
   end
 
   def test_nested_constants_are_referenced_by_their_complete_path
-    assert_equal __, AboutConstants::C
-    assert_equal __, ::AboutConstants::C
+    # Need to look into double-colon meanings, the second result here is strange.
+    # After digging into it, this would make a difference if C were defined 
+    # elsewhere in the code and we desired to use that as well.
+    assert_equal "nested", AboutConstants::C
+    assert_equal "nested", ::AboutConstants::C
   end
 
   # ------------------------------------------------------------------
@@ -35,7 +38,7 @@ class AboutConstants < Neo::Koan
   end
 
   def test_nested_classes_inherit_constants_from_enclosing_classes
-    assert_equal __, Animal::NestedAnimal.new.legs_in_nested_animal
+    assert_equal 4, Animal::NestedAnimal.new.legs_in_nested_animal
   end
 
   # ------------------------------------------------------------------
@@ -47,7 +50,7 @@ class AboutConstants < Neo::Koan
   end
 
   def test_subclasses_inherit_constants_from_parent_classes
-    assert_equal __, Reptile.new.legs_in_reptile
+    assert_equal 4, Reptile.new.legs_in_reptile
   end
 
   # ------------------------------------------------------------------
@@ -63,11 +66,17 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_both_nested_and_inherited_constants
-    assert_equal __, MyAnimals::Bird.new.legs_in_bird
+    assert_equal 2, MyAnimals::Bird.new.legs_in_bird
   end
 
   # QUESTION: Which has precedence: The constant in the lexical scope,
   # or the constant from the inheritance hierarchy?
+  # Lexical scope. Another oddity. This one took a bit to understand, with help
+  # from these two links:
+  # https://groups.google.com/forum/#!topic/comp.lang.ruby/t5rtDNol3P8
+  # http://stackoverflow.com/q/5464811/
+  # The scoping here is within the MyAnimals class, where LEGS is 2, which
+  # takes precedence over the LEGS value in Animals.
 
   # ------------------------------------------------------------------
 
@@ -78,10 +87,13 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_explicit_scoping_on_class_definition
-    assert_equal __, MyAnimals::Oyster.new.legs_in_oyster
+    assert_equal 4, MyAnimals::Oyster.new.legs_in_oyster
   end
 
   # QUESTION: Now which has precedence: The constant in the lexical
   # scope, or the constant from the inheritance hierarchy?  Why is it
   # different than the previous answer?
+  # See answer above, to start. This class is defined as related-to-MyAnimals
+  # but is not within the scope of the class definition of MyAnimals; it exists
+  # in the namespace of, but not as a child class of MyAnimals.
 end
