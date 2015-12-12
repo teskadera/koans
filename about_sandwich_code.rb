929debb97439faa72fc203eff39a862bc833757f
Robert <teskadera@gmail.com>
2015-12-11 22:32:16 -0800

    Finish AboutSandwichCode

--- about_sandwich_code.rb
+++ about_sandwich_code.rb
@@ -3,33 +3,33 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutSandwichCode < Neo::Koan
 
   def count_lines(file_name)
-    file = open(file_name)
+    file = open(file_name) # using a file handle
     count = 0
-    while file.gets
+    while file.gets # reading all the lines
       count += 1
     end
-    count
-  ensure
-    file.close if file
+    count # return count
+  ensure # odd, but neat that you can use implicit returns and ensure
+    file.close if file # close the file handle if it does not equal false/nil
   end
 
   def test_counting_lines
-    assert_equal __, count_lines("example_file.txt")
+    assert_equal 4, count_lines("example_file.txt")
   end
 
   # ------------------------------------------------------------------
 
   def find_line(file_name)
-    file = open(file_name)
-    while line = file.gets
-      return line if line.match(/e/)
+    file = open(file_name) # again, the handle/ensure-close structure
+    while line = file.gets # allows capture for each iteration
+      return line if line.match(/e/) # have to use explicit return
     end
   ensure
     file.close if file
   end
 
   def test_finding_lines
-    assert_equal __, find_line("example_file.txt")
+    assert_equal "test\n", find_line("example_file.txt")
   end
 
   # ------------------------------------------------------------------
@@ -47,6 +47,13 @@ class AboutSandwichCode < Neo::Koan
   # abstracting the top and bottom bread slices to a library can be
   # difficult in many languages.
   #
+  # - Coming from Java-land for the past couple of years, this is AOP, isn't
+  # - it? It seems like it, just without @-tags for wrapping methods. @Before
+  # - @After, @Transactional, @OneToOne, @Query... The file_sandwich method
+  # - reads exactly like an advice.
+  # http://docs.spring.io/spring/docs/current/spring-framework-reference/html/aop.html
+  # - They're extensively used in Java and make life much easier.
+  #
   # (Aside for C++ programmers: The idiom of capturing allocated
   # pointers in a smart pointer constructor is an attempt to deal with
   # the problem of sandwich code for resource allocation.)
@@ -56,9 +63,9 @@ class AboutSandwichCode < Neo::Koan
 
   def file_sandwich(file_name)
     file = open(file_name)
-    yield(file)
-  ensure
-    file.close if file
+    yield(file) # This resource is yielded into the block that is passed
+  ensure # implicit return on the last item in the block, then pass control
+    file.close if file # into here to close out the file
   end
 
   # Now we write:
@@ -74,17 +81,22 @@ class AboutSandwichCode < Neo::Koan
   end
 
   def test_counting_lines2
-    assert_equal __, count_lines2("example_file.txt")
+    assert_equal 4, count_lines2("example_file.txt")
   end
 
   # ------------------------------------------------------------------
 
   def find_line2(file_name)
-    # Rewrite find_line using the file_sandwich library function.
+    file_sandwich(file_name) do |file|
+      count = 0
+      while line = file.gets
+        return line if line.match(/e/)
+      end
+    end
   end
 
   def test_finding_lines2
-    assert_equal __, find_line2("example_file.txt")
+    assert_equal "test\n", find_line2("example_file.txt")
   end
 
   # ------------------------------------------------------------------
@@ -100,7 +112,7 @@ class AboutSandwichCode < Neo::Koan
   end
 
   def test_open_handles_the_file_sandwich_when_given_a_block
-    assert_equal __, count_lines3("example_file.txt")
+    assert_equal 4, count_lines3("example_file.txt")
   end
 
 end
